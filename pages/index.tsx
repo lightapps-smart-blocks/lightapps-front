7413dff30d4665d0a0b992f28841f9774929e4b0
lightapps-smart-blocks <133265751+lightapps-smart-blocks@users.noreply.github.com>
2023-11-06 22:27:32 -0300

    Commit from Stub AI (#3)
    Co-authored-by: lightapps-smart-blocks <ai@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,11 @@
-import Image from 'next/image';
 import Header from '../components/Header';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main className="flex flex-col items-center justify-center p-24">
-        <Carousel autoPlay infiniteLoop>
-          <div>
-            <img src="/images/app1.png" />
-            <p className="legend">App 1</p>
-          </div>
-          <div>
-            <img src="/images/app2.png" />
-            <p className="legend">App 2</p>
-          </div>
-          <div>
-            <img src="/images/app3.png" />
-            <p className="legend">App 3</p>
-          </div>
-        </Carousel>
+        <h1>Welcome to Home Page</h1>
       </main>
     </div>
   );
