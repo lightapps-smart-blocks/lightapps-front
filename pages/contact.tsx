03450aa2b46d3cfce7f195814f05878843a1b15d
Daniel Bonilha <daniel.bonilha@varsitytutors.com>
2023-11-06 23:05:06 -0300

    removed titles

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,7 +6,6 @@ export default function Contact() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex flex-col items-center justify-center p-24">
-        <h1>Contact Us</h1>
         <p className="mt-4 text-xl">Name: Daniel</p>
         <p className="mt-2 text-xl">Email: email@com</p>
       </main>
